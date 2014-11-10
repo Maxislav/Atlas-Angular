6dccd2bbbd2a122822726f30e5b8fdb11a3969d7
Maxislav <lmaxim@mail.ru>
2014-11-10 07:52:19 +0200

    Обработка формы входа

--- forum/js/app.js
+++ forum/js/app.js
@@ -1,11 +1,8 @@
 //Define an angular module for our app
 var forum = angular.module('forum', ['ngRoute']);
 
-//Define Routing for app
-//Uri /AddNewOrder -> template add_order.html and Controller AddOrderController
-//Uri /ShowOrders -> template show_orders.html and Controller AddOrderController
 forum.config(['$routeProvider',
-    function($routeProvider) {
+    function ($routeProvider) {
         $routeProvider.
             when('/isAuth', {
                 templateUrl: 'forum/html/isAuth.html',
@@ -21,22 +18,42 @@ forum.config(['$routeProvider',
     }]);
 
 
-forum.controller('noAuth', function($scope, $http) {
+forum.controller('noAuth', function ($scope, $http) {
     $scope.message = 'This is Add new order screen';
     $http.post('forum/php/isAuth', {mess: 'l'})
-        .success(function(data, status, headers, config){
+        .success(function (data, status, headers, config) {
             //callback(data)
         })
-        .error(function(data, status, headers, config) {
-           /* $scope.alertClass = 'show';
-            $scope.alertMess = 'Ошибка';
-            console.log(data)*/
+        .error(function (data, status, headers, config) {
+            /* $scope.alertClass = 'show';
+             $scope.alertMess = 'Ошибка';
+             console.log(data)*/
         });
+    $scope.yy = function () {
+        console.log('d')
+    }
 });
 
 
-forum.controller('isAuth', function($scope) {
-
+forum.controller('isAuth', function ($scope) {
     $scope.message = 'This is Show orders screen';
-
 });
+
+
+forum.controller('v', function ($scope) {
+    $scope.tmpl = 'name'
+    $scope.event = function () {
+        alert('f')
+    }
+    $scope.yy = function () {
+        console.log('d')
+    }
+})
+
+
+forum.directive('myCustomer', function () {
+    return  function ($scope, element) {
+        element.on('change',$scope.yy)
+    }
+})
+
