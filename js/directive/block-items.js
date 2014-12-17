39848df02c807a8ec3ee7de011ccf7fa654592fc
Maxislav <lmaxim@mail.ru>
2014-12-17 23:41:07 +0200

    Форум забросили.

--- js/directive/block-items.js
+++ js/directive/block-items.js
@@ -1,1 +1,10 @@
+app.directive('blockItem', function(windowSize){
+    var height = windowSize.height - 30
+    return{
+        restrict: 'C',
+        link: function(scope, el, attr){
+            el.css('height', height+ 'px')
+        }
+    }
+})
 
--- js/map.js
+++ js/map.js
@@ -1,5 +1,12 @@
 var app = angular.module('app', [])
 
+app.config( function( $httpProvider ) {    // [url]http://habrahabr.ru/post/181009/[/url]
+    $httpProvider.defaults.headers.post[ 'Content-Type' ] = 'application/x-www-form-urlencoded;charset=utf-8';
+    $httpProvider.defaults.transformRequest = function( data ) {
+        return angular.isObject( data ) && String( data ) !== '[object File]' ? angular.toParam( data ) : data;
+    };
+});
+
 app.constant('windowSize', {
     width: window.innerWidth,
     height: window.innerHeight
