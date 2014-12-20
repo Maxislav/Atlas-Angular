b4743550767079815aef03a9203b2a7189fdbd45
Maxislav <lmaxim@mail.ru>
2014-12-20 20:23:57 +0200

    валидация

---
+++ js/directive/directive-valid.js
@@ -0,0 +1,18 @@
+app.directive('valid', function(){
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                var transformedInput = text.replace(/[^0-9]/g, '');
+               // console.log(transformedInput);
+                if(transformedInput !== text) {
+                    ngModelCtrl.$setViewValue(transformedInput);
+                    ngModelCtrl.$render();
+                }
+                return transformedInput;  // or return Number(transformedInput)
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    }
+})
