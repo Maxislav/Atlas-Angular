25427a69b859960d0b1e0982d3b03671af4c7e03
Maxislav <lmaxim@mail.ru>
2014-12-21 12:34:33 +0200

    Инфо

--- js/controller/objects-contrl.js
+++ js/controller/objects-contrl.js
@@ -1,4 +1,12 @@
 app.controller('objectsContrl', function ($scope,factoryGetDevices){
     $scope.factoryGetDevices = factoryGetDevices;
-
+    $scope.current = {};
+    $scope.setCurrent = function(imei) {
+        for(var i=0; i<  $scope.factoryGetDevices.length; i++){
+            if($scope.factoryGetDevices[i].imei == imei){
+                $scope.current = $scope.factoryGetDevices[i];
+                break;
+            }
+        }
+    }
 })
