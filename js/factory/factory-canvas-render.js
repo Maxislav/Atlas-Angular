427c8973f44d5b39142f4be4488653334887fc79
Maxislav <lmaxim@mail.ru>
2015-01-02 21:31:26 +0200

    canvas render

--- js/factory/factory-canvas-render.js
+++ js/factory/factory-canvas-render.js
@@ -1,13 +1,26 @@
 app.factory('canvasRender', function(){
+
+    function getRadianAngle(degreeValue) {
+        return degreeValue * Math.PI / 180;
+    }
+
     function move(context,azimuth){
         context.clearRect(0,0,30,30);
         context.beginPath();
-        context.arc(30/2, 30/2, 7, 0, 2 * Math.PI, false);
+        context.moveTo(15,2);
+        context.lineTo(20,20);
+        context.lineTo(15,18);
+        context.lineTo(10,18);
+
+        context.rotate(getRadianAngle(parseFloat(azimuth)));
+
         context.fillStyle = 'blue';
         context.fill();
+        context.closePath();
         context.lineWidth = 1;
         context.strokeStyle = '#003300';
         context.stroke();
+
         console.log(azimuth+ ' degree')
     }
     function stop(context, color){
