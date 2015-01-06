12a458336b94e63a5184f3266ea0270d13fbc64a
Maxislav <lmaxim@mail.ru>
2015-01-06 23:38:31 +0200

    elapsed-post color

--- js/directive/main-map.js
+++ js/directive/main-map.js
@@ -2,30 +2,10 @@ app.directive('mainMap', function (windowSize, map) {
     return{
         restrict: 'C',
         link: function (scope, el, attr) {
-            var width = screen.height;
 
-            var height = screen.width;
 
-            var screenRatio;
-
-            var realWidth;
-            var realHeight;
-            if (isNaN(screenRatio)) {
-
-                if (window.innerHeight > window.innerWidth) {
-                    realWidth = window.innerHeight;
-                    realHeight = window.innerWidth;
-                    screenRatio = (window.innerWidth / window.innerHeight);
-                } else {
-                    realWidth = window.innerWidth;
-                    realHeight = window.innerHeight;
-                    screenRatio = (window.innerHeight / window.innerWidth);
-                }
-
-            }
-
-           alert(document.body.clientHeight);
-            el.css('height', windowSize.height + 'px');
+           //alert(document.body.clientHeight);
+            el.css('height', document.body.clientHeight + 'px');
             map.scope = scope
 
 
