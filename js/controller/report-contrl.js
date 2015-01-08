e3bc6ecbca74c28959714d7006dbce25ae7a5e59
Maxislav <lmaxim@mail.ru>
2015-01-08 11:22:51 +0200

    track get

--- js/controller/report-contrl.js
+++ js/controller/report-contrl.js
@@ -1,4 +1,4 @@
-app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevices', 'factoryFormatDate','$http', function ($scope, serviceShowElements, factoryGetDevices, factoryFormatDate, $http) {
+app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevices', 'factoryFormatDate', '$http', function ($scope, serviceShowElements, factoryGetDevices, factoryFormatDate, $http) {
     $scope.serviceShowElements = serviceShowElements;
 
     var d = new Date()
@@ -27,19 +27,21 @@ app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevi
         }
     })
 
-    $scope.showTrack = function(){
-        if($scope.devices && $scope.devices.current && $scope.devices.current.imei){
+    $scope.showTrack = function () {
+        if ($scope.devices && $scope.devices.current && $scope.devices.current.imei) {
             var b = $scope.before
-            var before = new Date(b.getFullYear(), b.getMonth(), b.getDate()+1);
+            var before = new Date(b.getFullYear(), b.getMonth(), b.getDate() + 1);
             var from = factoryFormatDate.dateToString($scope.after);
             var to = factoryFormatDate.dateToString(before);
-           $http
-               .post('php/showTrack.php',{
-               imei: $scope.devices.current.imei
-           })
-               .success(function(d){
-               console.log(d)
-           })
+            $http
+                .post('php/showTrack.php', {
+                    imei: $scope.devices.current.imei,
+                    from: from,
+                    to: to
+                })
+                .success(function (d) {
+                    console.log(d)
+                })
 
             //console.log(from+':'+to)
         }
