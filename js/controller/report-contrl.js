8b7d4873904e48c32ae9a061a7165523daf1f330
Maxislav <lmaxim@mail.ru>
2015-01-09 00:14:17 +0200

    track get

--- js/controller/report-contrl.js
+++ js/controller/report-contrl.js
@@ -1,13 +1,13 @@
-app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevices', 'factoryFormatDate', '$http', '$filter', function ($scope, serviceShowElements, factoryGetDevices, factoryFormatDate, $http, $filter) {
+app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevices', 'factoryFormatDate', '$http', '$filter','map', function ($scope, serviceShowElements, factoryGetDevices, factoryFormatDate, $http, $filter, map) {
     $scope.serviceShowElements = serviceShowElements;
-
+    var F = parseFloat;
     var d = new Date()
 
     $scope.before = new Date(d.getFullYear(), d.getMonth(), d.getDate())
     $scope.after = new Date(d.getFullYear(), d.getMonth(), d.getDate())
 
-    $scope.devices = factoryGetDevices
-    $scope.current
+    $scope.devices = factoryGetDevices;
+    $scope.current;
 
     $scope.$watch('devices.current', function () {
         $scope.devices.current && ($scope.currentImei = $scope.devices.current.imei);
@@ -29,7 +29,7 @@ app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevi
 
     $scope.showTrack = function () {
         if ($scope.devices && $scope.devices.current && $scope.devices.current.imei) {
-            var b = $scope.before
+            var b = $scope.before;
             var before = new Date(b.getFullYear(), b.getMonth(), b.getDate() + 1);
             var from = factoryFormatDate.dateToString($scope.after);
             var to = factoryFormatDate.dateToString(before);
@@ -40,14 +40,41 @@ app.controller('reportContrl', ['$scope', 'serviceShowElements', 'factoryGetDevi
                     to: to
                 })
                 .success(function (d) {
-                    //console.log(d)
                     var orderBy = $filter('orderBy');
-                    var arr = orderBy(d,'dateTime')
-                    console.log(arr)
+                    var arr = orderBy(d, 'dateTime');
+                    console.log(arr);
+                    var pointList = setArrLatLngs(arr);
+                    var polilyne =  L.polyline(pointList, {
+                        color: 'blue',
+                            weight: 5,
+                            opacity: 0.5,
+                        smoothFactor: 1
+
+                    });
+                   // firstpolyline.addTo(map.map)
+                    $scope.devices.current.trackGroup && map.map.removeLayer($scope.devices.current.trackGroup)
+                    $scope.devices.current.trackGroup = L.featureGroup([polilyne])
+                        .bindPopup('Hello world!')
+                        //.on('click', function() { alert('Clicked on a group!'); })
+
+                    $scope.devices.current.trackGroup.addTo(map.map);
+                   // $scope.current.trackGroup.
+
                 })
 
-            //console.log(from+':'+to)
         }
+    }
+
+    function setArrLatLngs (arr){
+        var arrLatLngs = []
+        for (var i = 0; i<arr.length; i++){
+            arrLatLngs.push([
+                F(arr[i].lat),
+               F(arr[i].lng)
+            ])
+        }
+        return arrLatLngs
 
     }
+
 }])
