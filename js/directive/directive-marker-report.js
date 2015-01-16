1754354fba55a8c4bba5893ed1e7061b43a595a8
Maxislav <lmaxim@mail.ru>
2015-01-16 14:50:31 +0200

    popup  на треке

--- js/directive/directive-marker-report.js
+++ js/directive/directive-marker-report.js
@@ -11,26 +11,22 @@ app
                 $scope.params = JSON.parse($scope.markerReport)
                 $element
                     .on('click', function () {
-                        var text = '<reportmarker>' + '</remoprmarker>'
-
+                        var text = '<reportpopup></reportpopup>'
                         var linkFn = $compile(text);
                         var content = linkFn($scope);
 
                         popup = L.popup({offset: [0, -10], minWidth:120})
                             .setLatLng([f($scope.params.lat), f($scope.params.lng)])
                             .setContent(content[0])
-
                             .addTo(map.map);
                     })
                     /*.on('mouseenter', function () {
                         console.log($scope.params)
                     })*/
-
-
             }
         }
     }])
-    .directive('reportmarker', function(){
+    .directive('reportpopup', function(){
         return{
             restrict: 'EA',
             templateUrl: 'item/report-popup.html',
