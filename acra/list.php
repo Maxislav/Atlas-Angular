f367978d9b93e2d78f7e6229d471e8b0be368451
Maxislav <lmaxim@mail.ru>
2015-07-06 14:31:02 +0300

    add acra ok

--- acra/list.php
+++ acra/list.php
@@ -1,5 +1,8 @@
 <?php
 $file = $_GET["file"];
 $content = "<h2 style='margin: 30px; color: rgb(113, 89, 155);'>".$_GET["file"]."</h2>";
-$content = $content."<div style='margin: 30px'>".file_get_contents( $file)."</div>";
+$content = $content."<div style='margin: 30px'>".file_get_contents($file)."</div>";
+
+$content = preg_replace("!<br />(.*?)<br />!si","<div>\\1</div>",$content);
+
 echo $content;
